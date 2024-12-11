54037e815f6c65c5d00cbc016e536a88e15b183a
Tran Quang Dat <tranquangdatquangtran@gmail.com>
2024-12-11 17:25:57 +0700

    Add UI boardcontent, Column, card

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,9 +1,14 @@
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
+const APP_BAR_HEIGHT = '68px'
+const BOARD_BAR_HEIGHT = '58px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
+
 const theme = extendTheme({
   trelloCustom: {
-    appBarHeight: '68px',
-    boardBarHeight: '58px',
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
     barPadding: 2
   },
   colorSchemes: {
@@ -31,6 +36,15 @@ const theme = extendTheme({
     MuiInputLabel: {
       styleOverrides: {
         root: { fontSize: '0.875rem' }
+      }
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          '&.MuiTypography-body1': {
+            fontSize: '0.875rem'
+          }
+        }
       }
     }
   }
