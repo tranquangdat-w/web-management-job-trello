7063981a309b5e22c320af8e2c8bb3dbf59604f7
Tran Quang Dat <tranquangdatquangtran@gmail.com>
2025-01-12 15:07:53 +0700

    add lib

--- frontend/src/pages/Boards/_id.jsx
+++ frontend/src/pages/Boards/_id.jsx
@@ -4,8 +4,11 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import mockData from '~/apis/mock-data'
+import { useState } from 'react'
 
 const Board = () => {
+  const [board, setBoard] = useState(null)
+
   return (
     <>
       <Container disableGutters={ true } maxWidth={ false } sx={{ height: '100vh' }}>
