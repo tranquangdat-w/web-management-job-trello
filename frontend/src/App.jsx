996e33d59550bbc98998e069c50193ab5afe0966
Phan Hai <phihaiphan2004@gmail.com>
2025-01-15 05:59:56 +0700

    sua log_up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import Board from '~/pages/Boards/_id'
+import LoginPage from '~/pages/Users/sign_in'
+import SignUpPage from '~/pages/Users/sign_up'
+import SignInPage from '~/pages/Users/sign_in'
+import BoardPage from '~/pages/BoardPage/boardPage'
+import Home from '~/pages/HomePages/home'
 
 const App = () => {
   return (
     <>
       {/* This is Board details not board list*/}
-      <Board />
+      <Home />
     </>
   )
 }
--- frontend/src/pages/Users/sign_in.jsx
+++ frontend/src/pages/Users/sign_in.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import { Box, Button, TextField, Typography, IconButton, InputAdornment, Link } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 
-function LoginPage() {
-  const [isSignIn, setIsSignIn] = useState(true);
+function SignInPage({ onSwitchToSignUp }) {
   const [passwordVisible, setPasswordVisible] = useState(false);
 
   const togglePasswordVisibility = () => {
@@ -32,17 +31,20 @@ function LoginPage() {
           padding: 3,
         }}
       >
-        <Typography variant="h4" component="h1" sx={{ mb: 2, ml : 35, }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2, ml: 35 }}>
           Sign in to<br />Trello easily
         </Typography>
-        <Typography variant="body1" sx={{ mb: 2, ml : 35, }}>
+        <Typography variant="body1" sx={{ mb: 2, ml: 35 }}>
           If you don’t have an account, register now. <br />
-          <Link href="#" onClick={() => setIsSignIn(false)} color="primary">
+          <Link href="#" onClick={onSwitchToSignUp} color="primary">
             Register here!
           </Link>
-        </Typography >
-        <img src="https://s3-alpha-sig.figma.com/img/feb1/4138/168afcca4345533683f89bb42220b2ef?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a2wSYz06xTL4qW5NosTgNm2z25e1hEpR7GSXSHr-6TCZK4cgvm6rl8be0E9tDIkrm5xkYH6ZlH2Vur9xCg8akt4dOwOK3a16x0~LYvlCmaVAtRFn15XRMUK4pk0j9W7FzAnXbJZbYeYCn4wiB9CPHWNtc~r7Cj5SYrExcdhWQNjxaZndA1iL7VJMXFGgUFg3dVon-BSA8W5itiJg-KyakphPeRk~F6E28DYkV~mkK1C8sT9sIt5Dhozu6zbFbCBpo5LHiHdnNi-wenEsxG5~ZorV8WBeS8amQKAH-hBR6OhUcHnvukJooUFfzw17mDMo4K-AiHtGXU15CWKpDTjfGw__" 
-        alt="Illustration" style={{ width: '250px', height: 'auto' , marginLeft: '400px', marginBottom: '-100px'}} />
+        </Typography>
+        <img
+          src="https://s3-alpha-sig.figma.com/img/feb1/4138/168afcca4345533683f89bb42220b2ef?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a2wSYz06xTL4qW5NosTgNm2z25e1hEpR7GSXSHr-6TCZK4cgvm6rl8be0E9tDIkrm5xkYH6ZlH2Vur9xCg8akt4dOwOK3a16x0~LYvlCmaVAtRFn15XRMUK4pk0j9W7FzAnXbJZbYeYCn4wiB9CPHWNtc~r7Cj5SYrExcdhWQNjxaZndA1iL7VJMXFGgUFg3dVon-BSA8W5itiJg-KyakphPeRk~F6E28DYkV~mkK1C8sT9sIt5Dhozu6zbFbCBpo5LHiHdnNi-wenEsxG5~ZorV8WBeS8amQKAH-hBR6OhUcHnvukJooUFfzw17mDMo4K-AiHtGXU15CWKpDTjfGw__"
+          alt="Illustration"
+          style={{ width: '250px', height: 'auto', marginLeft: '400px', marginBottom: '-100px' }}
+        />
       </Box>
 
       {/* Phần bên phải */}
@@ -55,110 +57,88 @@ function LoginPage() {
           padding: 3,
         }}
       >
-        {/* Đăng nhập */}
-        {isSignIn ? (
-          <Box sx={{ width: '100%', maxWidth: 400 }}>
-            <Typography variant="h5" component="h2" sx={{ mb: 2 }}>
-              Sign in
-            </Typography>
-            <form>
-              <TextField
-                fullWidth
-                label="Enter email or user name"
-                variant="outlined"
-                margin="normal"
-                required
-                sx={{ mb: 2, backgroundColor: '#F0EFFF', borderRadius: '5px' }}
-              />
-              <TextField
-                fullWidth
-                label="Password"
-                type={passwordVisible ? 'text' : 'password'}
-                variant="outlined"
-                margin="normal"
-                required
-                sx={{ mb: 2, backgroundColor: '#F0EFFF', borderRadius: '5px' }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton onClick={togglePasswordVisibility}>
-                        {passwordVisible ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <Link href="#" color="primary" sx={{ display: 'block', marginTop: 1 }}>
-                Forgot password?
-              </Link>
-              <Button fullWidth variant="contained" color="primary" sx={{
+        <Box sx={{ width: '100%', maxWidth: 400 }}>
+          <Typography variant="h5" component="h2" sx={{ mb: 2 }}>
+            Sign in
+          </Typography>
+          <form>
+            <TextField
+              fullWidth
+              label="Enter email or user name"
+              variant="outlined"
+              margin="normal"
+              required
+              sx={{ mb: 2, backgroundColor: '#F0EFFF', borderRadius: '5px' }}
+            />
+            <TextField
+              fullWidth
+              label="Password"
+              type={passwordVisible ? 'text' : 'password'}
+              variant="outlined"
+              margin="normal"
+              required
+              sx={{ mb: 2, backgroundColor: '#F0EFFF', borderRadius: '5px' }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={togglePasswordVisibility}>
+                      {passwordVisible ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Link href="#" color="primary" sx={{ display: 'block', marginTop: 1 }}>
+              Forgot password?
+            </Link>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              sx={{
                 backgroundColor: '#6C63FF',
                 color: '#fff',
                 padding: '10px',
-                }} type="submit">
-                Login
+              }}
+              type="submit"
+            >
+              Login
+            </Button>
+          </form>
+          <Box sx={{ mt: 2, textAlign: 'center' }}>
+            <Typography variant="body2" sx={{ mb: 1 }}>
+              or continue with
+            </Typography>
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+              <Button sx={{ color: 'white', "&:hover": { color: 'none' } }}>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/768px-Facebook_Logo_%282019%29.png"
+                  alt="Facebook"
+                  style={{ width: 30, height: 30, margin: '0 10px' }}
+                />
               </Button>
-            </form>
-            <Box sx={{ mt: 2, textAlign: 'center' }}>
-              <Typography variant="body2" sx={{ mb: 1 }}>
-                or continue with
-              </Typography>
-              <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/768px-Facebook_Logo_%282019%29.png" alt="Facebook" style={{ width: 30, height:30, margin: '0 10px' }} />
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCvh-j7HsTHJ8ZckknAoiZMx9VcFmsFkv72g&s" alt="Apple" style={{ width: 30, height:30, margin: '0 10px' }} />
-                <img src="https://cdn4.iconfinder.com/data/icons/logos-brands-7/512/google_logo-google_icongoogle-512.png" alt="Google" style={{ width: 30, height:30, margin: '0 10px' }} />
-              </Box>
+
+              <Button sx={{ color: 'white', "&:hover": { color: 'none' } }}>
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCvh-j7HsTHJ8ZckknAoiZMx9VcFmsFkv72g&s"
+                  alt="Apple"
+                  style={{ width: 30, height: 30, margin: '0 10px' }}
+                />
+              </Button>
+
+              <Button sx={{ color: 'white', "&:hover": { color: 'none' } }}>
+                <img
+                  src="https://cdn4.iconfinder.com/data/icons/logos-brands-7/512/google_logo-google_icongoogle-512.png"
+                  alt="Google"
+                  style={{ width: 30, height: 30, margin: '0 10px' }}
+                />
+              </Button>
             </Box>
           </Box>
-        ) : (
-          /* Đăng ký */
-          <Box sx={{ width: '100%', maxWidth: 400 }}>
-            <Typography variant="h5" component="h2" sx={{ mb: 2 }}>
-              Sign up
-            </Typography>
-            <form>
-              <TextField
-                fullWidth
-                label="Email"
-                variant="outlined"
-                margin="normal"
-                sx={{ mb: 2, backgroundColor: '#F0EFFF', borderRadius: '5px' }}
-                required
-              />
-              <TextField
-                fullWidth
-                label="Username"
-                variant="outlined"
-                margin="normal"
-                sx={{ mb: 2, backgroundColor: '#F0EFFF', borderRadius: '5px' }}
-                required
-              />
-              <TextField
-                fullWidth
-                label="Password"
-                type="password"
-                variant="outlined"
-                margin="normal"
-                sx={{ mb: 2, backgroundColor: '#F0EFFF', borderRadius: '5px' }}
-                required
-              />
-              <Button fullWidth variant="contained" color="primary" sx={{ mt: 2, backgroundColor: '#6C63FF',
-                color: '#fff',
-                padding: '10px',}} type="submit">
-                Register
-              </Button>
-            </form>
-            <Typography variant="body2" sx={{ textAlign: 'center', mt: 2 }}>
-              Already have an account?{' '}
-              <Link href="#" onClick={() => setIsSignIn(true)} color="primary">
-                Sign in here!
-              </Link>
-            </Typography>
-          </Box>
-        )}
+        </Box>
       </Box>
     </Box>
   );
 }
 
-export default LoginPage;
+export default SignInPage;
