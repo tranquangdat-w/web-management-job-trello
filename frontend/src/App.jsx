00ec25c70dcb4ea4e682ce4be945f63f31bbb3a1
Phan Hai <phihaiphan2004@gmail.com>
2025-01-16 10:14:40 +0700

    hai

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,31 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Board from '~/pages/Boards/_id';
-import { NotFound } from '~/pages/NotFound/NotFound';
-import { Auth } from './pages/Auth/Auth';
-import Home from '~/pages/HomePages/home'; 
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Board from '~/pages/Boards/_id'
+import { NotFound } from '~/pages/NotFound/NotFound'
+import { Auth } from './pages/Auth/Auth'
+
+// import BoardPage from '~/pages/BoardPage/boardPage'
+// import Home from '~/pages/HomePages/home'
+
 const App = () => {
   return (
     <Routes>
       {/* Redirect Route */}
       <Route path='/' element={
-        <Navigate to='/home' replace={true} />
+        <Navigate to='/boards/13be383a-75e4-4025-8de3-ad31c5d79500' replace={true} />
       }/>
 
-      {/* Home */}
-      <Route path='/home' element={<Home />} />
-
-      {/* Board Details */}
+      {/*Board Details*/}
       <Route path='/boards/:boardId' element={<Board />}/>
 
-      {/* Auth */}
+      {/*Auth*/}
       <Route path='/login' element={<Auth />} />
       <Route path='/register' element={<Auth />} />
 
       {/* 404 Not found */}
       <Route path='*' element={<NotFound />}/>
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
+
